a4be4a060250571575f9816f6f21079acfe80cd4
@aline.bora-kloppert <36476341+alineborak@users.noreply.github.com>
2022-12-05 19:01:16 +0100

    Creating a file with bugged code in c

---
+++ buggy.c
@@ -0,0 +1,30 @@
+// #include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    for (int i = 0;  i < 3; i++)
+    {
+        printf("i is %i\n", i);
+        printf("#\n");
+    }
+}
+
+// int get_negative_int(void);
+
+// int main(void)
+// {
+//     int i = get_negative_int();
+//     printf("%i\n", i);
+// }
+
+// int get_negative_int(void);
+// {
+//     int n;
+//     do
+//     {
+//         n = get_int("Negative Interget: ");
+//     } 
+//     while (n < 0);
+//     return n;
+//     }
