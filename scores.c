b0ead39b262e79937727a2e5104ab810c2722efb
@aline.bora-kloppert <36476341+alineborak@users.noreply.github.com>
2022-12-06 09:50:33 +0100

    scores and properties update

--- scores.c
+++ scores.c
@@ -1,10 +1,13 @@
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-    int score1 = 72;
-    int score2 = 73;
-    int score3 = 33;
+    int scores[3];
 
-    printf("Average: %f\n", (score1 + score2 + score3) / 3.0);
+    scores[0] = get_int("Score: ");
+    scores[1] = get_int("Score: ");
+    scores[2] = get_int("Score: ");
+
+    printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / 3.0);
 }
